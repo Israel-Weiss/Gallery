9539876da19d87a00398ade17a2b548a0d8415f5
Israel Weiss <93487387+Israel-Weiss@users.noreply.github.com>
2022-08-02 00:30:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 'use strict'
 
-const gNames = ['Sokoban', 'MineSweeper', 'Space invaders', 'Calculator', 'Touch Nums', 'Ball Board']
-const gTiles = ['Push boxes', 'Mine sweeper', 'Kill aliens',
-    'Simple calculator', 'Find out numbers', 'Eliminate balls']
+const gNames = ['Sokoban', 'Space invaders', 'Ball Board', 'Calculator', 'Touch Nums', 'MineSweeper']
+const gRfers = ['Sokoban', 'Space-Invaders', 'Ball-board', 'Calculator', 'Touch-nums', 'Mine-sweeper']
 
-const gProjects = createProject(gNames, gTiles);
+const gTiles = ['Push boxes', 'Kill aliens', 'Eliminate balls',
+    'Simple calculator', 'Find out numbers', 'Mine sweeper']
 
-function createProject(names, tiles) {
+const gProjects = createProject(gNames, gTiles, gRfers);
+
+function createProject(names, tiles, refers) {
     var projects = []
     for (var i = 0; i < names.length; i++) {
         projects[i] = {}
         projects[i].name = names[i]
         projects[i].title = tiles[i]
+        projects[i].href = refers[i]
     }
     return projects
 }
@@ -20,8 +23,8 @@ renderPortfolio(gProjects)
 
 function renderPortfolio(projects) {
     var strHTML = ''
-for (var i = 0; i < projects.length; i++) {
-    strHTML += `<div class="col-md-4 col-sm-6 portfolio-item">
+    for (var i = 0; i < projects.length; i++) {
+        strHTML += `<div class="col-md-4 col-sm-6 portfolio-item">
     \n<a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
     \n<div class="portfolio-hover" onclick="renderModal(${i})">
     \n<div class="portfolio-hover-content">
@@ -35,46 +38,46 @@ for (var i = 0; i < projects.length; i++) {
     \n<p class="text-muted">${projects[i].title}</p>
     \n</div>
     \n</div>`
-}
+    }
     document.querySelector('.projects').innerHTML = strHTML
 }
 
 function renderModal(idx) {
-    var strHtml = 
-    `<div class="modal-dialog">
-    <div class="modal-content">
-    <div class="close-modal" data-dismiss="modal">
-    <div class="lr">
-    <div class="rl"></div>
-    </div>
-    </div>
-    <div class="container">
-    <div class="row">
-    <div class="col-lg-8 mx-auto">
-    <div class="modal-body">
-    <h2>${gProjects[idx].name}</h2>
-    <p class="item-intro text-muted">${gProjects[idx].title}.</p>
-    <img class="img-fluid d-block mx-auto" src="img/portfolio/${idx + 1}.png" alt="">
-    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-    blanditiis
-    dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae
-    cupiditate,
-    maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-    <ul class="list-inline">
-    <li>Date: January 2017</li>
-    <li>Client: Threads</li>
-    <li>Category: Illustration</li>
-    </ul>
-    <button class="btn btn-primary" data-dismiss="modal" type="button">
-    <i class="fa fa-times"></i>
-    Close Project</button>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>`
-        document.querySelector('.portfolio-modal').innerHTML = strHtml              
+    var strHtml =
+        `<div class="modal-dialog">
+    \n<div class="modal-content">
+    \n<div class="close-modal" data-dismiss="modal">
+    \n<div class="lr">
+    \n<div class="rl"></div>
+    \n</div>
+    \n</div>
+    \n<div class="container">
+    \n<div class="row">
+    \n<div class="col-lg-8 mx-auto">
+    \n<div class="modal-body">
+    \n<h2>${gProjects[idx].name}</h2>
+    \n<p class="item-intro text-muted">${gProjects[idx].title}.</p>
+    \n<a href="https://israel-weiss.github.io/${gProjects[idx].href}/"><img class="img-fluid d-block mx-auto" src="img/portfolio/${idx + 1}.png" alt=""></a>
+    \n<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+    \nblanditiis
+    \ndolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae
+    \ncupiditate,
+    \nmaiores repudiandae, nostrum, reiciendis facere nemo!</p>
+    \n<ul class="list-inline">
+    \n<li>Date: January 2017</li>
+    \n<li>Client: Threads</li>
+    \n<li>Category: Illustration</li>
+    \n</ul>
+    \n<button class="btn btn-primary" data-dismiss="modal" type="button">
+    \n<i class="fa fa-times"></i>
+    \nClose Project</button>
+    \n</div>
+    \n</div>
+    \n</div>
+    \n</div>
+    \n</div>
+    \n</div>`
+    document.querySelector('.portfolio-modal').innerHTML = strHtml
 }
 
 
